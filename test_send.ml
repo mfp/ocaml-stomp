d4c5d93c31d92100f9b1c33c86c88e16a29c54ed
Mauricio Fernandez <mfp@acm.org>
2009-04-28 12:44:31 +0200

    test_send: --newline option.

--- test_send.ml
+++ test_send.ml
@@ -12,6 +12,7 @@ let queue = ref (sprintf "/queue/test-%d" (Random.int 100000000))
 let login = ref None
 let passcode = ref None
 let num_queues = ref None
+let use_nl_eof = ref false
 
 let msg = "Usage: test [options]"
 
@@ -28,22 +29,24 @@ let args =
       "--queues", String (set_some_f int_of_string num_queues), "N Send to N queues.";
       "--login", String (set_some login), "LOGIN Use the given login (default: none).";
       "--passcode", String (set_some passcode), "PASSCODE Use the given passcode (default: none).";
+      "--newline", Set use_nl_eof, " Use \\0\\n to signal end of frame (default: no).";
     ]
 
 let () =
   Arg.parse args ignore msg;
-  let c = S.connect ?login:!login ?passcode:!passcode
+  let c = S.connect ?login:!login ?passcode:!passcode ~eof_nl:!use_nl_eof
             (Unix.ADDR_INET (Unix.inet_addr_of_string !address, !port)) in
   let queue_name i = match !num_queues with
       None -> !queue
     | Some n -> String.concat "-" [!queue; string_of_int (i mod n)]
   in
     begin match !num_queues with
-        None -> printf "Sending to %s-0\n" !queue
+        None -> printf "Sending to %s\n" !queue
       | Some n -> printf "Sending to %d queues of prefix %s-\n" n !queue
     end;
     for i = 1 to !num_msgs do
       printf "\r%d%!" i;
       S.send_no_ack c (queue_name i) (string_of_int i)
     done;
+    print_newline ();
     S.disconnect c
