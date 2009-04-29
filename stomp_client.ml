029e1588370dca5a09bf499b7580c79c4d4e17ed
Mauricio Fernandez <mfp@acm.org>
2009-04-29 11:10:32 +0200

    Reorganized Stomp_client module types: BASE, GENERIC, HIGH_LEVEL.

--- stomp_client.ml
+++ stomp_client.ml
@@ -12,32 +12,61 @@ type stomp_error =
 
 exception Stomp_error of string * stomp_error
 
-module type BASIC =
+module type BASE =
 sig
   type 'a thread
   type connection
   type transaction
   type message_id
 
-  val connect : ?login:string -> ?passcode:string -> ?eof_nl:bool ->
-    ?headers:(string * string) list -> Unix.sockaddr -> connection thread
-  val disconnect : connection -> unit thread
-  val send : connection -> ?transaction:transaction -> ?persistent:bool ->
-    destination:string -> ?headers:(string * string) list -> string -> unit thread
-  val send_no_ack : connection -> ?transaction:transaction ->
-    destination:string -> ?headers:(string * string) list -> string -> unit thread
-
-  val receive_msg : connection -> received_msg thread
-  val ack_msg : connection -> ?transaction:transaction -> received_msg -> unit thread
-
-  val subscribe : connection -> ?headers:(string * string) list -> string -> unit thread
-  val unsubscribe : connection -> ?headers:(string * string) list -> string -> unit thread
-
   val transaction_begin : connection -> transaction thread
   val transaction_commit : connection -> transaction -> unit thread
   val transaction_commit_all : connection -> unit thread
   val transaction_abort_all : connection -> unit thread
   val transaction_abort : connection -> transaction -> unit thread
+
+  val receive_msg : connection -> received_msg thread
+  val ack_msg : connection -> ?transaction:transaction -> received_msg -> unit thread
+
+  val disconnect : connection -> unit thread
+end
+
+module type GENERIC =
+sig
+  include BASE
+
+  val connect : ?login:string -> ?passcode:string -> ?eof_nl:bool ->
+    ?headers:(string * string) list -> Unix.sockaddr -> connection thread
+  val send : connection -> ?transaction:transaction -> ?persistent:bool ->
+    destination:string -> ?headers:(string * string) list -> string -> unit thread
+  val send_no_ack : connection -> ?transaction:transaction ->
+    destination:string -> ?headers:(string * string) list -> string -> unit thread
+
+  val subscribe : connection -> ?headers:(string * string) list -> string -> unit thread
+  val unsubscribe : connection -> ?headers:(string * string) list -> string -> unit thread
+end
+
+module type HIGH_LEVEL =
+sig
+  include BASE
+
+  val connect :
+    Unix.sockaddr -> ?prefetch:int -> login:string -> passcode:string ->
+    connection thread
+
+  val send : connection -> ?transaction:transaction -> ?persistent:bool ->
+    destination:string -> string -> unit thread
+
+  val topic_send : connection -> ?transaction:transaction -> ?persistent:bool ->
+    destination:string -> string -> unit thread
+
+  val topic_send_no_ack : connection -> ?transaction:transaction -> ?persistent:bool ->
+    destination:string -> string -> unit thread
+
+  val subscribe_queue : connection -> string -> unit thread
+  val unsubscribe_queue : connection -> string -> unit thread
+  val subscribe_topic : connection -> string -> unit thread
+  val unsubscribe_topic : connection -> string -> unit thread
 end
 
 module Make(C : Concurrency_monad.THREAD) =
