6779662a3e2ccbefc7476970a09a698befb94850
Mauricio Fernandez <mfp@acm.org>
2009-04-30 10:42:49 +0200

    --transaction option in test_send_rabbitmq.

--- test_send_rabbitmq.ml
+++ test_send_rabbitmq.ml
@@ -12,6 +12,7 @@ let dest = ref (sprintf "test-%d" (Random.int 100000000))
 let login = ref ""
 let passcode = ref ""
 let is_topic = ref false
+let use_transaction = ref false
 
 let msg = "Usage: test_send_rabbitmq [options]"
 
@@ -25,21 +26,26 @@ let args =
       "--login", Set_string login, "LOGIN Use the given login (default: none).";
       "--passcode", Set_string passcode, "PASSCODE Use the given passcode (default: none).";
       "--topic", Set is_topic, " Send as a topic message (default: no).";
+      "--transaction", Set use_transaction, " Send all messages in a transaction.";
     ]
 
 let () =
   Arg.parse args ignore msg;
   let c = S.connect !login !passcode
-            (Unix.ADDR_INET (Unix.inet_addr_of_string !address, !port))
+            (Unix.ADDR_INET (Unix.inet_addr_of_string !address, !port)) in
+  let transaction = match !use_transaction with
+      true -> Some (S.transaction_begin c)
+    | false -> None
   in
     printf "Sending to %s\n" !dest;
     for i = 1 to !num_msgs do
       let msg = sprintf "message number %d" i in
         printf "\r%d%!" i;
         if !is_topic then
-          S.topic_send_no_ack c !dest msg
+          S.topic_send_no_ack c ?transaction ~destination:!dest msg
         else
-          S.send_no_ack c !dest msg
+          S.send_no_ack c ?transaction ~destination:!dest msg
     done;
     print_newline ();
+    S.transaction_commit_all c;
     S.disconnect c
