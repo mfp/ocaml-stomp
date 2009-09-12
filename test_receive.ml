0483a69414e333c54a2dbc439f60b413babc8e36
Mauricio Fernandez <mfp@acm.org>
2009-09-12 16:47:16 +0200

    test_receive: --header option, better rate display.

--- test_receive.ml
+++ test_receive.ml
@@ -18,6 +18,7 @@ let ack = ref false
 let verbose = ref false
 let readsubs = ref false
 let durable = ref false
+let headers = ref []
 
 let msg = "Usage: test_receive [options]"
 
@@ -34,6 +35,7 @@ let args =
       "--stdin", Set readsubs, " Read list of destinations to from stdin.";
       "--ack", Set ack, " Send ACKs for received messages.";
       "--durable", Set durable, " Create durable destinations in RabbitMQ.";
+      "--header", String (fun s -> headers := s :: !headers), " Use custom header in SUBSCRIBE.";
       "--login", String (set_some login), "LOGIN Use the given login (default: none).";
       "--passcode", String (set_some passcode), "PASSCODE Use the given passcode (default: none).";
       "--newline", Set use_nl_eof, " Use \\0\\n to signal end of frame (default: no).";
@@ -60,29 +62,37 @@ let () =
   let payload = ref 0 in
   let print_rate () =
     let dt = Unix.gettimeofday () -. !t0 in
-      printf "\n\nReceived %d messages in %.1fs (%8.1f/s)\n" 
+      printf "\n\nReceived %d messages in %.1fs (%8.1f/s)\n"
         !cnt dt (float !cnt /. dt);
       printf "Total payload %d KB (%d KB/s).\n" (!payload / 1024)
         (truncate (float !payload /. 1024. /. dt));
       S.disconnect c;
       exit 1 in
   let subs = if !readsubs then !dests @ read_subs () else !dests in
-  let headers =
-    if !durable then ["auto-delete", "false"; "durable", "true"] else [] in
-  let headers = if !ack then ("ack", "client") :: headers else headers in
+  let hs =
+    List.filter_map
+      (fun s -> try Some (String.split s ":") with _ -> None) !headers in
+  let hs =
+    if !durable then ["auto-delete", "false"; "durable", "true"] @ hs
+    else hs in
+  let hs = if !ack then ("ack", "client") :: hs else hs in
     Sys.set_signal Sys.sigint (Sys.Signal_handle (fun _ -> print_rate ()));
-    if !verbose then
-      printf "Subscribing to %d destination(s)... %!" (List.length subs);
-    List.iter (S.subscribe ~headers c) subs;
-    if !verbose then printf "DONE\n%!";
+    printf "Subscribing to %d destination(s)... %!" (List.length subs);
+    if !verbose then printf "\n";
+    List.iteri
+      (fun i dst ->
+         if !verbose && i mod 10 = 0 then printf "%d\r%!" i;
+         S.subscribe ~headers:hs c dst)
+      subs;
+    printf "DONE             \n%!";
     (try
       for i = 1 to !num_msgs do
-        let msg = S.receive_msg c in 
+        let msg = S.receive_msg c in
           incr cnt;
           payload := !payload + String.length msg.Mq.msg_body;
           if i = 1 then t0 := Unix.gettimeofday ();
           if !ack then S.ack_msg c msg;
-          if !verbose then printf "\r%d%!" i;
+          if !verbose && i mod 11 = 0 then printf "\r%d%!" i;
       done;
      with Exit -> ());
     print_rate ()
